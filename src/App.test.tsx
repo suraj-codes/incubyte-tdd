c59c67654070df6ed288d848b7c3868e78614d70
suraj-humancloud <suraj.deshmane@humancloud.co.in>
2025-01-17 11:28:17 +0530

    Add failing test to check Calculator renders with initial display '0'

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import Calculator from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Calculator with initial display "0"', () => {
+  render(<Calculator />);
+  const displayElement = screen.getByText("0");
+  expect(displayElement).toBeInTheDocument();
 });
